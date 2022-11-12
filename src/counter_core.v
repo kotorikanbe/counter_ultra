a7e8b4acf93595fb5211d6b155fc77cf78aee6f4
7zzd <a126686439@gmail.com>
2022-11-12 15:15:42 +0800

    trying

--- src/counter_core.v
+++ src/counter_core.v
@@ -14,28 +14,40 @@ module counter_core
     reg [3:0] data_4=4'b0000;
     reg [3:0] data_5=4'b0000;
     reg [3:0] data_6=4'b0000;
-    assign min_o={data_2,data_1};
-    assign sec_o={data_4,data_3};
-    assign ms_10_o={data_6,data_5};
+    reg [7:0]min_o;
+    reg [7:0]sec_o;
+    reg [7:0]ms_10_o;
     always @(posedge clk_core or negedge rst) begin
         if(en)begin
                 if (data_1<4'b1001) begin
                     data_1<=data_1+1'b1;
+                    ms_10_o<={data_2,data_1};
+                    sec_o<={data_4,data_3};
+                    min_o<={data_6,data_5};
                 end
                 else if(data_2<4'b1001) begin
                     data_2<=data_2+1'b1;
                     data_1<=4'b0000;
+                    ms_10_o<={data_2,data_1};
+                    sec_o<={data_4,data_3};
+                    min_o<={data_6,data_5};
                 end
                 else if(data_3<4'b1001) begin
                     data_3<=data_3+1'b1;
                     data_1<=4'b0000;
                     data_2<=4'b0000;
+                    ms_10_o<={data_2,data_1};
+                    sec_o<={data_4,data_3};
+                    min_o<={data_6,data_5};
                 end
                 else if(data_4<4'b0101) begin
                     data_4<=data_4+1'b1;
                     data_1<=4'b0000;
                     data_2<=4'b0000;
                     data_3<=4'b0000;
+                    ms_10_o<={data_2,data_1};
+                    sec_o<={data_4,data_3};
+                    min_o<={data_6,data_5};
                 end
                 else if(data_5<4'b1001) begin
                     data_5<=data_5+1'b1;
@@ -43,6 +55,9 @@ module counter_core
                     data_2<=4'b0000;
                     data_3<=4'b0000;
                     data_4<=4'b0000;
+                    ms_10_o<={data_2,data_1};
+                    sec_o<={data_4,data_3};
+                    min_o<={data_6,data_5};
                 end
                 else if(data_6<4'b0101) begin
                     data_6<=data_6+1'b1;
@@ -51,6 +66,9 @@ module counter_core
                     data_3<=4'b0000;
                     data_4<=4'b0000;
                     data_5<=4'b0000;
+                    ms_10_o<={data_2,data_1};
+                    sec_o<={data_4,data_3};
+                    min_o<={data_6,data_5};
                 end
                 else begin
                 data_1<=4'b0000;
@@ -59,6 +77,9 @@ module counter_core
                 data_4<=4'b0000;
                 data_5<=4'b0000;
                 data_6<=4'b0000;
+                ms_10_o<={data_2,data_1};
+                sec_o<={data_4,data_3};
+                min_o<={data_6,data_5};
             end
         end
         else if(!rst)begin
@@ -68,6 +89,9 @@ module counter_core
                 data_4<=4'b0000;
                 data_5<=4'b0000;
                 data_6<=4'b0000;
+                ms_10_o<={data_2,data_1};
+                sec_o<={data_4,data_3};
+                min_o<={data_6,data_5};
             end
     end
 endmodule
