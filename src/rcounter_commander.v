8eae29ec71c11b443fcfef55ff186499c3d9edbc
7zzd <a126686439@gmail.com>
2022-11-14 17:24:42 +0800

    rcounter done

--- src/rcounter_commander.v
+++ src/rcounter_commander.v
@@ -18,15 +18,23 @@ reg [7:0] ms_10_o;
 reg [1:0] target=2'b01;
 reg  curr_state=1'b0;
 reg  next_state=1'b0;
-reg [7:0] min_i=8'b00000000;
-reg [7:0] sec_i=8'b00000000;
+reg [7:0] min_i=8'b00010000;
+reg [7:0] sec_i=8'b00010000;
 reg [7:0] ms_10_i=8'b00000000;
 wire [7:0] min_o_rcounter;
 wire [7:0] sec_o_rcounter;
 wire [7:0] ms_10_o_rcounter;
 reg switch=1'b0;
+reg [1:0]next_target=2'b01;
+reg [7:0] next_min_i;
+reg [7:0] next_sec_i;
+reg [7:0] next_ms_10_i;
 always @(posedge clk_core) begin
     curr_state<=next_state;
+    target<=next_target;
+    min_i<=next_min_i;
+    sec_i<=next_sec_i;
+    ms_10_i<=next_ms_10_i;
 end
 //0为置数&暂停状态，1为开始计时状态
 always @(*) begin
@@ -49,61 +57,60 @@ always @(*) begin
     if(!curr_state)
         begin
             switch=1'b0;
-            target=2'b01;
             case ({left_button,right_button,up_button,down_button})
                 4'b1000: begin
                     if(target<2'b10)begin
-                        target=target+1'b1;
+                        next_target=target+1'b1;
                     end
                     else ;
                 end
                 4'b0100: begin
                     if(target>2'b00) begin
-                        target=target-1'b1;
+                        next_target=target-1'b1;
                     end
                 end
                 4'b0010:begin
                     case (target)
                         2'b00: begin
                             if(ms_10_i[3:0]<4'b1001)begin
-                                ms_10_i=ms_10_i+1'b1;
+                                next_ms_10_i=ms_10_i+1'b1;
                             end
                             else if(ms_10_i[7:4]<4'b1001)begin
-                                ms_10_i[3:0]=4'b0000;
-                                ms_10_i[7:4]=ms_10_i+1'b1;
+                                next_ms_10_i[3:0]=4'b0000;
+                                next_ms_10_i[7:4]=ms_10_i[7:4]+1'b1;
                             end
                             else begin
-                                ms_10_i=8'b00000000;
+                                next_ms_10_i=8'b00000000;
                             end
                         end
                         2'b01: begin
                             if(sec_i[3:0]<4'b1001)begin
-                                sec_i=sec_i+1'b1;
+                                next_sec_i=sec_i+1'b1;
                             end
-                            else if(sec_i[7:4]<4'b1001)begin
-                                sec_i[3:0]=4'b0000;
-                                sec_i[7:4]=sec_i+1'b1;
+                            else if(sec_i[7:4]<4'b0110)begin
+                                next_sec_i[3:0]=4'b0000;
+                                next_sec_i[7:4]=sec_i[7:4]+1'b1;
                             end
                             else begin
-                                sec_i=8'b00000000;
+                                next_sec_i=8'b00000000;
                             end
                         end
                         2'b10:begin
                             if(min_i[3:0]<4'b1001)begin
-                                min_i=min_i+1'b1;
+                                next_min_i=min_i+1'b1;
                             end
                             else if(min_i[7:4]<4'b1001)begin
-                                min_i[3:0]=4'b0000;
-                                min_i[7:4]=min_i+1'b1;
+                                next_min_i[3:0]=4'b0000;
+                                next_min_i[7:4]=min_i[7:4]+1'b1;
                             end
                             else begin
-                                min_i=8'b00000000;
+                                next_min_i=8'b00000000;
                             end
                         end
                         default: begin
-                            min_i=min_i;
-                            sec_i=sec_i;
-                            ms_10_i=ms_10_i;
+                            next_min_i=min_i;
+                            next_sec_i=sec_i;
+                            next_ms_10_i=ms_10_i;
                         end
                     endcase
                 end
@@ -111,52 +118,52 @@ always @(*) begin
                     case (target)
                         2'b00: begin
                             if(ms_10_i[3:0]>4'b0000) begin
-                                ms_10_i[3:0]=ms_10_i[3:0]-1'b1;
+                                next_ms_10_i[3:0]=ms_10_i[3:0]-1'b1;
                             end
                             else if(ms_10_i[7:4]>4'b0000)begin
-                                ms_10_i[3:0]=4'b1001;
-                                ms_10_i[7:4]=ms_10_i[7:4]-1'b1;
+                                next_ms_10_i[3:0]=4'b1001;
+                                next_ms_10_i[7:4]=ms_10_i[7:4]-1'b1;
                             end
                             else begin
-                                ms_10_i=8'b10011001;
+                                next_ms_10_i=8'b10011001;
                             end
                         end
                         2'b01: begin
                             if(sec_i[3:0]>4'b0000) begin
-                                sec_i[3:0]=sec_i[3:0]-1'b1;
+                                next_sec_i[3:0]=sec_i[3:0]-1'b1;
                             end
                             else if(sec_i[7:4]>4'b0000)begin
-                                sec_i[3:0]=4'b1001;
-                                sec_i[7:4]=sec_i[7:4]-1'b1;
+                                next_sec_i[3:0]=4'b1001;
+                                next_sec_i[7:4]=sec_i[7:4]-1'b1;
                             end
                             else begin
-                                sec_i=8'b10011001;
+                                next_sec_i=8'b01101001;
                             end
                         end
                         2'b10:begin
                             if(min_i[3:0]>4'b0000) begin
-                                min_i[3:0]=min_i[3:0]-1'b1;
+                                next_min_i[3:0]=min_i[3:0]-1'b1;
                             end
                             else if(min_i[7:4]>4'b0000)begin
-                                min_i[3:0]=4'b1001;
-                                min_i[7:4]=min_i[7:4]-1'b1;
+                                next_min_i[3:0]=4'b1001;
+                                next_min_i[7:4]=min_i[7:4]-1'b1;
                             end
                             else begin
-                                min_i=8'b10011001;
+                                next_min_i=8'b10011001;
                             end
                         end
                         default: begin
-                            min_i=min_i;
-                            sec_i=sec_i;
-                            ms_10_i=ms_10_i;
+                            next_min_i=min_i;
+                            next_sec_i=sec_i;
+                            next_ms_10_i=ms_10_i;
                         end
                     endcase
                 end
                 default: begin
-                    min_i=min_i;
-                    sec_i=sec_i;
-                    ms_10_i=ms_10_i;
-                    target=target;
+                    next_min_i=min_i;
+                    next_sec_i=sec_i;
+                    next_ms_10_i=ms_10_i;
+                    next_target=target;
                 end
             endcase
             min_o=min_i;
@@ -170,14 +177,15 @@ always @(*) begin
             sec_o=sec_o_rcounter;
             ms_10_o=ms_10_o_rcounter;
             if(center_button)begin
-                min_i=min_o_rcounter;
-                sec_i=sec_o_rcounter;
-                ms_10_i=ms_10_o_rcounter;                
+                next_min_i=min_o_rcounter;
+                next_sec_i=sec_o_rcounter;
+                next_ms_10_i=ms_10_o_rcounter;
+                next_target=2'b01;                
             end
         end
 end
 rcounter_core main_rcounter(
-    .clk_core(clk_core),
+    .clk_core(~clk_core),
     .min_i(min_i),
     .ms_10_i(ms_10_i),
     .sec_i(sec_i),
